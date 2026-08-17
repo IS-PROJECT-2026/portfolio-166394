e73048629914e8c0608c6f4ba8f0d2bfa3bfbd27
Sasha Miriti <sashammiriti@gmail.com>
2026-08-17 13:53:41 +0300

    feat: add Projects section content and interaction
    Closes #5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("Portfolio site loaded");
+
+document.querySelectorAll('.project').forEach(project => {
+  project.addEventListener('click', () => {
+    project.classList.toggle('expanded');
+  });
+});
